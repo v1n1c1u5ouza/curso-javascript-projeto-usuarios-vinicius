915227bf6512617025851a755ce7cd19889857c9
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-19 12:57:08 -0300

    Using querySelector to get form information

---
+++ index.js
@@ -0,0 +1,8 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name-gender]:checked");
+var birth = document.querySelector("#exampleInputBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var pass = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admin = document.  querySelector("#exampleInputAdmin");
