4b948b9870045a6b30df80a9987e9997bf2095a4
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-20 08:46:00 -0300

    Add:forEach to get form information

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ var email = document.querySelector("#exampleInputEmail");
 var pass = document.querySelector("#exampleInputPassword");
 var photo = document.querySelector("#exampleInputFile");
 var admin = document.  querySelector("#exampleInputAdmin");
+
+var fields = document.querySelectorAll("#form-user-create [name]");
+
+fields.forEach(function(field, index){
+
+    if(field.name == "gender") {
+
+        if(field.checked) {
+
+            console.log("sim", field);
+
+        }
+
+    } else {
+
+        console.log("não");
+
+    }
+
+    //console.log(field.id, field.name, field.value, field.checked, index);
+
+})
