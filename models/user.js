7a67af5112fab630dcfe517bfc4b4ccb5dc9dde5
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-20 11:53:25 -0300

    Create class User

---
+++ models/user.js
@@ -0,0 +1,16 @@
+class User {
+
+    constructor(name, gender, birth, country, email, password, photo, adimin) {
+
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this. country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.adimin = adimin;
+
+    }
+
+}
