9c9cba95975e14f322f4561e02ea89a3ab843304
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-20 11:07:15 -0300

    Add: function to add form information to the layout

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
+function addline(dataUser){
+
+    var tr = document.createElement("tr");
+
+    tr.innerHTML = `
+    
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+    
+    `;
+
+    document.getElementById("table-users").appendChild(tr);
+
+}
+
 document.getElementById("form-user-create").addEventListener("submit", function (Event){
 
     Event.preventDefault();
@@ -21,7 +45,6 @@ document.getElementById("form-user-create").addEventListener("submit", function 
     
     });
 
-    console.log(user);
+    addline(user);
 
 });
-
