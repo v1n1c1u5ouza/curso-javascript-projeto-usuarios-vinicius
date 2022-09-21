f6ab340d00187d5a767083c37081dc9471515f2a
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-21 13:01:55 -0300

    Fix: misspelling

--- classes/utiils.js
+++ classes/utiils.js
@@ -2,7 +2,7 @@ class Utils {
 
     static dateFormat(date){
 
-        return date.getdate()+'/'+(date.getmouth()+1)+'/'+date.getFullYear()+''+date.getHours()+':'+date.getMinutes();
+        return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' ' + date.getHours() + ':' + date.getMinutes();
 
     }
 
