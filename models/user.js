5e26ada4d54d576950b19a66e11eaa525190d7d2
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-21 12:16:06 -0300

    Create getters to attributes

--- models/user.js
+++ models/user.js
@@ -2,15 +2,46 @@ class User {
 
     constructor(name, gender, birth, country, email, password, photo, admin) {
 
-        this.name = name;
-        this.gender = gender;
-        this.birth = birth;
-        this. country = country;
-        this.email = email;
-        this.password = password;
-        this.photo = photo;
-        this.admin = admin;
+        this._name = name;
+        this._gender = gender;
+        this._birth = birth;
+        this._country = country;
+        this._email = email;
+        this._password = password;
+        this._photo = photo;
+        this._admin = admin;
 
     }
 
+    get name(){
+        this._name;
+    }
+
+    get gender(){
+        this._gender;
+    }
+
+    get birth(){
+        this._birth;
+    }
+
+    get country(){
+        this._country;
+    }
+
+    get email(){
+        this._email;
+    }
+
+    get password(){
+        this._password;
+    }
+
+    get photo(){
+        this._photo;
+    }
+
+    get admin(){
+        this._admin;
+    }
 }
