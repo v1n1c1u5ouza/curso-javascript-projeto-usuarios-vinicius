c3035c4a9bfe741529956107efd10240cfab24b2
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-22 08:56:20 -0300

    Add: has-error to form fields

--- controllers/userController.js
+++ controllers/userController.js
@@ -85,8 +85,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+
+            if (['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
+
+                field.parentElement.classList.add("has-error");
+                isValid = false
+
+            }
 
             if (field.name === "gender") {
 
@@ -105,6 +113,10 @@ class UserController {
             }
 
         });
+
+        if (!isValid) {
+            return false;
+        }
 
         return new User(
             user.name, 
