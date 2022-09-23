19b24d4563085559b1c906dd41e1069191a078ec
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-23 12:29:01 -0300

    Add: Method to delete info from localStorage

--- models/user.js
+++ models/user.js
@@ -124,11 +124,15 @@ class User {
 
     getNewID() {
 
-        if (!window.id) window.id = 0;
+        let usersID = parseInt(localStorage.getItem("usersID"));
 
-        id++;
+        if (!usersID > 0) usersID = 0;
 
-        return id
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
 
     }
 
@@ -156,6 +160,24 @@ class User {
 
             users.push(this);
         }
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+    remove(){
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index) => {
+            
+            if (this._id == userData._id) {
+
+                users.splice(index, 1);
+
+            }
+
+        });
 
         localStorage.setItem("users", JSON.stringify(users));
 
